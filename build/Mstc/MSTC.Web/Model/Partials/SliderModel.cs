895a211d44c00c3266715056d9b2655b83c4854e
Mike Hook <mike@hooktechnologies.co.uk>
2020-10-23 09:34:55 +0100

    Added Image folder and files.
    Updated Home template with slider.

---
+++ build/Mstc/MSTC.Web/Model/Partials/SliderModel.cs
@@ -0,0 +1,10 @@
+﻿namespace MSTC.Web.Model.Partials
+{
+    public class SliderModel
+    {
+        public string LinkUrl { get; set; }
+        public string CssClass { get; set; }
+        public string LinkText { get; set; }
+        public string SubText { get; set; }
+    }
+}
