e35fc35fa9fdae0f11226ad9fd90e4269b86ca52
Mike Hook <mike@hooktechnologies.co.uk>
2020-11-13 12:11:27 +0000

    Added BlogMiniListing.

---
+++ build/Mstc/MSTC.Web/Model/Partials/BlogMiniModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MSTC.Web.Model.Partials
+{
+    public class BlogMiniModel
+    {
+        public string Title { get; set; }
+        public int StartNodeId { get; set; }
+        public int NumberOfPosts { get; set; }
+    }
+}
