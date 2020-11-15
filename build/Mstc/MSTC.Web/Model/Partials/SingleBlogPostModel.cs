61261522fecd789feff1b45fa7160e84325e8283
Mike Hook <mike@hooktechnologies.co.uk>
2020-11-15 11:39:31 +0000

    Updated styling on Blog components, added SingleBlogPost partial.

---
+++ build/Mstc/MSTC.Web/Model/Partials/SingleBlogPostModel.cs
@@ -0,0 +1,11 @@
+﻿using Umbraco.Web.PublishedContentModels;
+
+namespace MSTC.Web.Model.Partials
+{
+    public class SingleBlogPostModel
+    {
+        public Blogpost Post { get; set; }
+        public bool ShowArchiveLink { get; set; }
+        public bool ShowPermaLink { get; set; }
+    }
+}
