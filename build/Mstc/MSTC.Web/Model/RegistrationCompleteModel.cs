6030c816f0de08d2ea7503d1e0d0a89662248814
Mike Hook <mike@hooktechnologies.co.uk>
2020-11-29 20:22:06 +0000

    Fixed up the registration complete postback. Use the TempData as umbraco fudges up the model binding.

--- build/Mstc/MSTC.Web/Model/RegistrationCompleteModel.cs
+++ build/Mstc/MSTC.Web/Model/RegistrationCompleteModel.cs
@@ -4,11 +4,17 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 using Umbraco.Core.Models;
+using Umbraco.Web.PublishedContentModels;
 
 namespace MSTC.Web.Model
 {
     public class RegistrationCompleteModel
     {
+        public RegistrationCompleteModel()
+        {
+
+        }
+
         public bool PromptForConfirmation { get; set; }
         public string PaymentDescription { get; set; }
         public string Cost { get; set; }
