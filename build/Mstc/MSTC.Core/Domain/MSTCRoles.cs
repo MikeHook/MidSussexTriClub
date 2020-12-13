045a786349026e61984ea0a2030d24655f6bf602
Mike Hook <mike@hooktechnologies.co.uk>
2020-12-13 14:54:21 +0000

    Finished MemberOptions model and GET action.

--- build/Mstc/MSTC.Core/Domain/MSTCRoles.cs
+++ build/Mstc/MSTC.Core/Domain/MSTCRoles.cs
@@ -9,6 +9,8 @@ namespace Mstc.Core.Domain
     public static class MSTCRoles
     {
         public static string Member = "Member";
+        public static string MemberAdmin = "MemberAdmin";
+        public static string Coach = "Coach";
         public static string Guest = "Guest";
 
     }
