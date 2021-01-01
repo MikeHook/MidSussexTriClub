123c1b53eeda48e853a3f25f33cacafee3afe2e4
Mike Hook <mike@hooktechnologies.co.uk>
2021-01-01 14:46:28 +0000

    Added MemberAdmin template.

---
+++ build/Mstc/MSTC.Web/Controllers/MemberAdminController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using Mstc.Core.Dto;
+using Mstc.Core.Providers;
+using Umbraco.Web.WebApi;
+
+namespace MSTC.Web.Controllers
+{
+	[MemberAuthorize(AllowGroup = "MemberAdmin")]
+	public class MemberAdminController : UmbracoApiController
+    {
+		MemberProvider _memberProvider;
+
+		public MemberAdminController()
+		{
+			_memberProvider = new MemberProvider(Services);
+		}
+
+        public IEnumerable<MemberDetailsDto> Get()
+        {
+			try
+			{
+				return _memberProvider.GetAllMemberDetails();					
+			}
+			catch (Exception ex)
+			{
+				Logger.Error(typeof(MemberAdminController), string.Format("Exception getting MemberAdmin members: {0}", ex.ToString()), ex);
+				return new List<MemberDetailsDto>();
+			}
+        }
+
+		
+	}
+}
