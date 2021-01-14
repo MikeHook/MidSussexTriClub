027a39dbdbbb47a14c8d0c3f4d934c0ffed40d2d
Mike Hook <mike@hooktechnologies.co.uk>
2021-01-14 17:38:33 +0000

    Started EventBooking implementation.

---
+++ build/Mstc/MSTC.Core/Dto/EventSlot.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mstc.Core.Dto
+{
+    public class EventSlot
+    {
+        public DateTime eventDate { get; set; }
+        public int participants { get; set; }
+        public decimal cost { get; set; }
+    }
+}
