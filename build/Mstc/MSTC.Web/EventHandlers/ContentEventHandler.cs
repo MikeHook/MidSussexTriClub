dc314863cd0467552928aee57e8b108a29cd8891
Mike Hook <mike@hooktechnologies.co.uk>
2021-01-17 15:24:56 +0000

    Updating event slots when an event is saved.

--- build/Mstc/MSTC.Web/EventHandlers/ContentEventHandler.cs
+++ build/Mstc/MSTC.Web/EventHandlers/ContentEventHandler.cs
@@ -7,6 +7,7 @@ using Mstc.Core;
 using Mstc.Core.DataAccess;
 using Mstc.Core.Domain;
 using Mstc.Core.Providers;
+using Umbraco.Core.Events;
 using Umbraco.Core.Models;
 using Umbraco.Web;
 using Umbraco.Web.PublishedContentModels;
@@ -28,14 +29,6 @@ namespace MSTC.Web.EventHandlers
 
         public void ContentService_Saving(Umbraco.Core.Services.IContentService sender, Umbraco.Core.Events.SaveEventArgs<IContent> e)
         {
-            /*
-             *-	Add any slots which do not exist
-                -	Check if any slots should be removed
-	                - If slots have participants then cancel saving with message
-	                - If slots are empty then remove the slots
-                -	Update cost and max participants on existing slots
-             */
-
             List<EventType> eventTypes = _dataTypeProvider.GetEventTypes();            
             foreach (IContent entity in e.SavedEntities)
             {
@@ -50,47 +43,24 @@ namespace MSTC.Web.EventHandlers
                         //Log and move on
                         continue;
                     }
-                    
-                    if (eventPage.Recurring)
+
+                    List<EventSlot> existingEventSlots = _eventSlotRepository.GetAll(true).ToList();
+                    if (existingEventSlots.Any())
                     {
-                        var eventDates = new List<DateTime>();
-                        var eventDate = eventPage.StartDate;
-                        while (eventDate <= eventPage.EndDate)
+                        //If slots have participants then cancel saving with message
+                        if (existingEventSlots.Any(es => es.EventParticipants.Count > 0))
                         {
-                            eventDates.Add(eventDate);
-                            if (eventPage.RecurringFrequency == (int)RecurringFrequencyEnum.Weekly)
-                            {
-                                eventDate = eventDate.AddDays(7);
-                            }
-                            else
-                            {
-                                eventDate = eventDate.AddMonths(1);
-                            }
+                            e.Messages.Add(new EventMessage("Error", "Unable to save changes - existing bookings for event slots, please cancel the bookings first.", EventMessageType.Error));
+                            e.Cancel = true;
+                            return;
                         }
 
-                        foreach(var date in eventDates)
-                        {
-                            var eventSlot = new EventSlot()
-                            {
-                                EventTypeId = eventTypeId.Value,
-                                Date = date,
-                                Cost = eventPage.Cost,
-                                MaxParticipants = eventPage.MaximumParticipants
-                            };
-                            _eventSlotRepository.Create(eventSlot);
-                        }
+                        UpdateEventSlots(existingEventSlots, eventTypeId.Value, eventPage);
                     }
                     else
                     {
-                        var eventSlot = new EventSlot()
-                        {
-                            EventTypeId = eventTypeId.Value,
-                            Date = eventPage.StartDate,
-                            Cost = eventPage.Cost,
-                            MaxParticipants = eventPage.MaximumParticipants
-                        };
-                        _eventSlotRepository.Create(eventSlot);
-                    }                    
+                        AddEventSlots(eventTypeId.Value, eventPage);
+                    }
                 }
             }   
         }
@@ -103,6 +73,58 @@ namespace MSTC.Web.EventHandlers
 	                - If slots are empty then remove the slots
              */
             //throw new NotImplementedException();
-        }      
+        }   
+        
+        private void AddEventSlots(int eventTypeId, Event eventPage)
+        {
+            if (eventPage.Recurring)
+            {
+                var eventDates = new List<DateTime>();
+                var eventDate = eventPage.StartDate;
+                while (eventDate <= eventPage.EndDate)
+                {
+                    eventDates.Add(eventDate);
+                    if (eventPage.RecurringFrequency == (int)RecurringFrequencyEnum.Weekly)
+                    {
+                        eventDate = eventDate.AddDays(7);
+                    }
+                    else
+                    {
+                        eventDate = eventDate.AddMonths(1);
+                    }
+                }
+
+                foreach (var date in eventDates.Where(d => d > DateTime.Now)) //Only add events in the future
+                {
+                    var eventSlot = new EventSlot()
+                    {
+                        EventTypeId = eventTypeId,
+                        Date = date,
+                        Cost = eventPage.Cost,
+                        MaxParticipants = eventPage.MaximumParticipants
+                    };
+                    _eventSlotRepository.Create(eventSlot);
+                }
+            }
+            else
+            {
+                var eventSlot = new EventSlot()
+                {
+                    EventTypeId = eventTypeId,
+                    Date = eventPage.StartDate,
+                    Cost = eventPage.Cost,
+                    MaxParticipants = eventPage.MaximumParticipants
+                };
+                _eventSlotRepository.Create(eventSlot);
+            }        
+        }
+
+        private void UpdateEventSlots(List<EventSlot> existingEventSlots, int eventTypeId, Event eventPage)
+        {
+            //TODO - Implement this
+            //Add any slots which do not exist
+            //Remove any slots which are no longer needed
+            //Update existing slots max participants and cost
+        }
     }
 }
