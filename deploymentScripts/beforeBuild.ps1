0e20fed49cec4220db22a1ea9e3735c459db2ebe
Mike Hook <mike@hooktechnologies.co.uk>
2021-02-28 15:27:51 +0000

    Updated top nav to hide unless logged in. Added beforebuild script.

---
+++ deploymentScripts/beforeBuild.ps1
@@ -0,0 +1,27 @@
+#Use this command to run the script
+#&("D:\dev\MSTC\deploymentScripts\beforeBuild.ps1")
+
+Write-Host 'Running before build script'
+
+#$root = 'D:\Temp'
+#$root = 'D:\dev\MidSussexTriathlon\build\MidSussexTriClub.Web'
+$root =  $env:APPVEYOR_BUILD_FOLDER + '\build\MidSussexTriClub.Web'
+Write-Host $root
+
+$configPath = $root + '\Web.config'
+$webconfig = get-content $configPath
+#Write-Host $webconfig
+
+function ReplaceAppSetting($sourceFile, $keyName, $replacementValue) {
+    $sourceFile -replace "<add key=`"$keyName`" value=`".*`"","<add key=`"$keyName`" value=`"$replacementValue`""
+}
+
+$webconfig = $webconfig -replace "connectionString=`"([^`"]+)`"","connectionString=`"$env:databaseConnectionString`""
+$webconfig = ReplaceAppSetting $webconfig "gmailUserName" $env:gmailUserName
+$webconfig = ReplaceAppSetting $webconfig "gmailPassword" $env:gmailPassword
+$webconfig = ReplaceAppSetting $webconfig "gocardlessEnvironment" $env:gocardlessEnvironment
+$webconfig = ReplaceAppSetting $webconfig "environment" $env:gocardlessEnvironment
+
+[System.IO.File]::WriteAllLines($configPath, $webconfig)
+
+Write-Host 'Finished before packaging script'
