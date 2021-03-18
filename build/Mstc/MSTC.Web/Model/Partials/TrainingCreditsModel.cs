84d2b2d4eb75f1f33f4a5d0de5df7d0f74b993a4
Mike Hook <mike@hooktechnologies.co.uk>
2021-03-18 17:16:34 +0000

    Fix for training credits errors not showing.

--- build/Mstc/MSTC.Web/Model/Partials/TrainingCreditsModel.cs
+++ build/Mstc/MSTC.Web/Model/Partials/TrainingCreditsModel.cs
@@ -10,7 +10,9 @@ namespace MSTC.Web.Model.Partials
     {
         public int CurrentTrainingCredits { get; set; }
 
-        [Required, Display(Name = "Buy credits"), Range(1, 30)]
+        [Required, Display(Name = "Buy credits"), Range(1, 50)]
         public int TrainingCreditsToBuy { get; set; }
+
+        public string EventBookingPageUrl { get; set; }
     }
 }
