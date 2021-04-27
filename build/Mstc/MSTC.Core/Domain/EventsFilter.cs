3c92825f392bfcb48a5dcb4af60624bac095cf8f
Mike Hook <mike@hooktechnologies.co.uk>
2021-04-27 09:07:54 +0100

    Filter event booking to only show events for booking from the next day onwards.

---
+++ build/Mstc/MSTC.Core/Domain/EventsFilter.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel;
+
+namespace Mstc.Core.Domain
+{
+	public enum EventsFilter
+    {        
+        AllEvents = 0,
+        CurrentAndFutureEvents = 1,
+        FutureEvents = 2
+    }
+}
